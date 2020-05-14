c43f1336a42e1b050e8cf3851c72923aca14406e
Hsin Ling Hu <hsinkava11@gmail.com>
2020-05-13 21:55:25 -0700

    revised

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export default {
-    API_ENDPOINT:process.env.REACT_APP_API_ENDPOINT || `http://localhost:8000`
+    API_ENDPOINT:'http://localhost:8000/api'
 }
+
+//process.env.REACT_APP_API_ENDPOINT || `https://obscure-sierra-79316.herokuapp.com/api`||
